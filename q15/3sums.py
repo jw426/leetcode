7a4308756b63f046b3cf14c9a4c1e4786c484fd9
jw426 <jiwonbae010426@gmail.com>
2024-03-04 23:47:59 +1100

    unfinished q15, problem with large list with 0s

---
+++ q15/3sums.py
@@ -0,0 +1,49 @@
+from bisect import bisect_left
+
+class Solution(object):
+
+    def bsearch(self, nums, find) -> int:
+        i = bisect_left(nums, find)
+        if i != len(nums) and nums[i] == find:
+            return i
+        return -1
+
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        sumlst = []
+
+        # nums is now sorted 
+        nums.sort()
+
+        # add two numbers in array
+        # find if number needed to make 0 is in the array
+        total = len(nums)
+        for a1 in range(0, total):
+            for a2 in range(a1 + 1, total):
+                need = -(nums[a1] + nums[a2])
+
+                # binary search of needed number
+                idx = self.bsearch(nums, need)
+                
+                # add all repeat occurrences of needed number
+                if idx != -1:
+                    while idx < total and nums[idx] == need:
+                        if idx > a2 and [nums[a1], nums[a2], nums[idx]] not in sumlst:
+                            sumlst.append([nums[a1], nums[a2], nums[idx]])
+                                
+                        idx += 1
+        
+        return sumlst 
+
+obj = Solution()
+nums = [-1,0,1,2,-1,-4]
+print(obj.threeSum(nums))
+
+
+
+
+        
