b3d19ec34e3e50162c629e87a9319647636eba59
jw426 <jiwonbae010426@gmail.com>
2024-03-12 14:41:52 +1100

    does not pass all - problem with line 35

---
+++ q15-16/4sums.py
@@ -0,0 +1,48 @@
+class Solution(object):
+
+    # modified from threeSumClosest in 
+    # 3sums_cloeset.py in current folder
+    def fourSum(self, nums, target):
+
+        nums.sort()
+        res = []     # we return this array
+        
+        # n1
+        for i in range(len(nums)):
+            # n2
+            for j in range(i+1, len(nums) - 2):
+                # n3, n4
+                low, high = j+1, len(nums) - 1
+
+                # target can never be achieved with current i and j
+                if target < nums[i] + sum(nums[j:j+3]):
+                    break
+                if target > nums[i] + sum(nums[-3:]):
+                    break
+
+                # disregard repeating 
+                if j > 0 and nums[j] == nums[j-1]:
+                    continue
+
+                while low < high:
+                    curcomb = [nums[i], nums[j], nums[low], nums[high]]
+                    if sum(curcomb) > target:
+                        high -= 1
+                    elif sum(curcomb) < target:
+                        low += 1
+                    else:
+                        res.append(curcomb)
+                        break
+
+            # disregard repeating 
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+        return curcomb
+
+obj = Solution()
+nums = [1,0,-1,0,-2,2]
+target = 0
+print(obj.fourSum(nums, target))
+                
+        
