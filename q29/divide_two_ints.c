db786cca0ca1325313cf7bd8ae7b4c324b651ee5
jw426 <jiwonbae010426@gmail.com>
2024-03-28 15:36:28 +1100

    solution using bitwise shift, in 32-bit int

--- q29/divide_two_ints.c
+++ q29/divide_two_ints.c
@@ -1,24 +1,29 @@
 #include <math.h>
+#include <stdlib.h>
 
 int divide(int dividend, int divisor) {
 
     // edge cases 
+    if (divisor == dividend) return 1;
     if (divisor == 1) return dividend;
     if (divisor == -1) {
-        if (dividend == -2147483648) return 2147483647;
+        if (dividend == INT_MIN) return INT_MAX;
         else return 0-dividend;
     }
 
-    int quotient = 0;
+    unsigned int quotient = 0;
     int positive = dividend < 0 == divisor < 0;  // flag for sign of quotient 
 
-    unsigned int ndividend = abs(dividend);
-    unsigned int ndivisor = abs(divisor);
+    unsigned int ndividend = dividend == INT_MIN ? INT_MAX + 1 : abs(dividend);
+    unsigned int ndivisor = divisor == INT_MIN ? INT_MAX + 1 : abs(divisor);
+
     while (ndividend >= ndivisor) {
-        ndividend -= ndivisor;
-        quotient++;
+        short bt = 0;
+        while (ndividend > (ndivisor << (bt+1))) bt++;
+        quotient += (1 << bt);
+        ndividend -= ndivisor << bt;
     }
 
-    if (positive) return quotient;
-    return 0 - quotient;
+    if (quotient == INT_MAX && positive) return INT_MAX;
+    return positive ? quotient : 0 - quotient;
 }
