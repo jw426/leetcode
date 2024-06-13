2795b2be7ddbdc328f028d1fab32f08cb27e6b88
jw426 <jiwonbae010426@gmail.com>
2024-06-13 14:42:32 +1000

    sudoku efficient impl

--- q35-39/q36_sudoku.py
+++ q35-39/q36_sudoku.py
@@ -1,35 +1,37 @@
+from collections import defaultdict
 class Solution:
     DIGIT = 9
     GRID = 3
 
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        row = ["" for _ in range(self.DIGIT)]
-        col = ["" for _ in range(self.DIGIT)]
-        box = ["" for _ in range(self.DIGIT)]
+        # hardcoding is most efficient
+        # since sudoku is 9x9
+
+        row = defaultdict(set)
+        col = defaultdict(set)
+        sqr = defaultdict(set)
 
         for i_row in range(self.DIGIT):
             for i_col in range(self.DIGIT): 
                 cell = board[i_row][i_col]
+                
+                if cell == '.':
+                    continue 
+
                 if (self.repeated(row, i_row, cell) or 
                     self.repeated(col, i_col, cell) or
-                    self.repeated(box, (i_row // self.GRID) * self.GRID + i_col // self.GRID, cell)):
-                    print(row, i_row)
-                    print(col, i_col)
-                    print(box)
-                    print(cell)
+                    self.repeated(sqr, (i_row // self.GRID, i_col // self.GRID), cell)):
                     return False
+                
+        return True
     
     def repeated(self, block, i, add_new):
 
-        if add_new == '.':
-            return False
-
         if add_new in block[i]:
-            print(add_new, block[i])
             return True
 
-        block[i] += add_new
+        block[i].add(add_new)
         
 
 
